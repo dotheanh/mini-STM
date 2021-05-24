da4cd9f6e2dfcea54cdf2dd5774945ce5d99e045
The Anh <nhoxtheanh@gmail.com>
2021-05-24 10:59:05 +0700

    add move right & move down animation for all monster

--- src/modules/game/Monster.js
+++ src/modules/game/Monster.js
@@ -33,16 +33,9 @@ var WalkingMonster = Monster.extend({
     ctor: function(xPos, yPos, zOrder, scaleRate) {
         this._super(xPos, yPos, zOrder, scaleRate);
         this._flyable = false;
-    }
-});
 
-var Assassin = WalkingMonster.extend({
-    ctor: function(xPos, yPos, zOrder, scaleRate) {
-        this._super(xPos, yPos, zOrder, scaleRate);
-        this._speed = 20;
-
-        cc.spriteFrameCache.addSpriteFrames(monster_res.assassin_plist, monster_res.assassin_png);
-        cc.animationCache.addAnimations(monster_res.assassin_anim_plist);
+        cc.spriteFrameCache.addSpriteFrames(this._plist, this._png);
+        cc.animationCache.addAnimations(this._anim_plist);
 
 		this._move_down_animation = cc.animationCache.getAnimation("move_down");
         this._move_down_animation.setLoops(-1);
@@ -54,24 +47,37 @@ var Assassin = WalkingMonster.extend({
 		this._move_right_action = cc.animate(this._move_right_animation);
         this._move_right_action.retain();
     },
-    getFileName: function() {
-        return battle_res.monster_assassin_run_0000;
-    },
     moveDown: function() {
         this._super();
-        this._img.stopAction(this._move_down_action);
+        this._img.stopAction(this._move_right_action);//////////////////////////////
+        this._img.stopAction(this._move_down_action);//////////////////////////////
         this._img.runAction(this._move_down_action);
     },
-
     moveRight: function() {
         this._super();
-        this._img.stopAction(this._move_down_action);
-        this._img.stopAction(this._move_right_action);
+        this._img.stopAction(this._move_down_action);//////////////////////////////
+        this._img.stopAction(this._move_right_action);//////////////////////////////
         this._img.runAction(this._move_right_action);
+    }
+});
+
+var Assassin = WalkingMonster.extend({
+    ctor: function(xPos, yPos, zOrder, scaleRate) {
+        this._plist = monster_res.assassin_plist;
+        this._png = monster_res.assassin_png;
+        this._anim_plist = monster_res.assassin_anim_plist;
+        this._super(xPos, yPos, zOrder, scaleRate);
+        this._speed = 20;
+    },
+    getFileName: function() {
+        return battle_res.monster_assassin_run_0000;
     }
 });
 var DarkGiant = WalkingMonster.extend({
     ctor: function(xPos, yPos, zOrder, scaleRate) {
+        this._plist = monster_res.dark_giant_plist;
+        this._png = monster_res.dark_giant_png;
+        this._anim_plist = monster_res.dark_giant_anim_plist;
         this._super(xPos, yPos, zOrder, scaleRate);
         this._speed = 10;
     },
@@ -81,6 +87,9 @@ var DarkGiant = WalkingMonster.extend({
 });
 var Iceman = WalkingMonster.extend({
     ctor: function(xPos, yPos, zOrder, scaleRate) {
+        this._plist = monster_res.iceman_plist;
+        this._png = monster_res.iceman_png;
+        this._anim_plist = monster_res.iceman_anim_plist;
         this._super(xPos, yPos, zOrder, scaleRate);
         this._speed = 14;
     },
@@ -94,10 +103,42 @@ var FlyingMonster = Monster.extend({
     ctor: function(xPos, yPos, zOrder, scaleRate) {
         this._super(xPos, yPos, zOrder, scaleRate);
         this._flyable = true;
+
+        cc.spriteFrameCache.addSpriteFrames(this._plist, this._png);
+        cc.animationCache.addAnimations(this._anim_plist);
+
+		this._move_down_animation = cc.animationCache.getAnimation("move_down");
+        this._move_down_animation.setLoops(-1);
+		this._move_down_action = cc.animate(this._move_down_animation);
+        this._move_down_action.retain();
+
+        this._move_right_animation = cc.animationCache.getAnimation("move_right");
+        this._move_right_animation.setLoops(-1);
+		this._move_right_action = cc.animate(this._move_right_animation);
+        this._move_right_action.retain();
     },
+    moveDown: function() {
+        this._super();
+        this._img.stopAction(this._move_right_action);
+        this._img.stopAction(this._move_down_action);
+        this._img.runAction(this._move_down_action);
+    },
+    moveRight: function() {
+        this._super();
+        this._img.stopAction(this._move_down_action);
+        this._img.stopAction(this._move_right_action);
+        this._img.runAction(this._move_right_action);
+    }
 });
 
 var Bat = FlyingMonster.extend({
+    ctor: function(xPos, yPos, zOrder, scaleRate) {
+        this._plist = monster_res.bat_plist;
+        this._png = monster_res.bat_png;
+        this._anim_plist = monster_res.bat_anim_plist;
+        this._super(xPos, yPos, zOrder, scaleRate);
+        this._speed = 25;
+    },
     getFileName: function() {
         return battle_res.monster_bat_run_0000;
     }
